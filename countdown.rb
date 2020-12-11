00a6a870db57f872aa50ef7360381e8a337f5800
courserav <lvsmlit@gmail.com>
2020-12-10 19:22:29 -0500

    commit tree 99855714686f63b8fdedcf93f24fa4a93f9fc3c9

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,27 @@
 #write your code here
 
-def countdown
+def countdown(count)
+    while count > 0
+        puts "#{count} SECOND(S)!"
+        count -= 1
+    end
+
+    if count == 0
+        return "HAPPY NEW YEAR!"
+    end
 end
+
+puts countdown(10)
+
+def countdown_with_sleep(count)
+    while count > 0
+        puts "#{count} SECOND(S)!"
+        count -= 1
+        
+        sleep(1)
+    end
+    #wanted to make sure the 'if' statement wasn't necessary. 
+     return "HAPPY NEW YEAR!"
+end
+
+puts countdown_with_sleep(5)
